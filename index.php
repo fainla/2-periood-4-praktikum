68f2d8cfba69e3c596e70d785634fc2a3e823d49
fainla <ainlapoiss@hot.ee>
2014-10-20 09:09:10 +0300

    initial

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+   <head>
+		<title>Praktikum 4</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		
+   </head>
+   <body>
+	<form action="index.php" method="post">
+		<span>Kasutajanimi: </span>
+		<input type="text" name="username"><br>
+		<span>Parool: </span>
+		<input type="password" name="password"><br>
+		<input type="submit" name="submit"><br>
+	</form>
