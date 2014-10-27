649aaffc1c9f1b54072a461a0c0fcbeaf441447e
fainla <ainlapoiss@hot.ee>
2014-10-27 12:55:26 +0200

    finnal

--- index.php
+++ index.php
@@ -1,15 +1,37 @@
-<!DOCTYPE HTML>
+<?php 
+		if (isset($_POST["submit"])) {
+			$username = $_POST['username'];
+			$password = $_POST['password'];
+			$message = "parool on vale";
+
+		echo "<style>.pwclass{border: 1px solid orange;}</style>"; }
+		else {
+		$message = "Palun logi sisse";}
+
+	 ?>
+
+<!DOCTYPE html>
 <html>
-   <head>
-		<title>Praktikum 4</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		
-   </head>
-   <body>
-	<form action="index.php" method="post">
-		<span>Kasutajanimi: </span>
-		<input type="text" name="username"><br>
-		<span>Parool: </span>
-		<input type="password" name="password"><br>
-		<input type="submit" name="submit"><br>
-	</form>
+<head>
+	<title>praktikum 2.4</title>
+</head>
+<body>
+
+		<h2><?php echo $message;?></h2>
+			<form action="index.php" method="post">
+
+
+				text<input type="text" name="username" value="<?php if (isset($_POST["submit"])) { $username = $_POST['username'];
+
+				 echo $username;}?>"></input>
+
+				password<input type="password" name="password" class="passwordclass" ></input>
+
+				<input type="submit" value="Saada" name="submit"></input>
+
+
+			</form>
+		<pre><?php print_r($_POST);?></pre>
+
+</body>
+</html>
